c4f283f3dd86ef200f40bf199976097cafff1b17
KsArT-IT <ksart.it@gmail.com>
2024-12-07 23:21:44 +0200

    feat: add code day 7 task 2

---
+++ 07/code_2.swift
@@ -0,0 +1,99 @@
+//
+//  main.swift
+//  2024d0721
+//
+//  Created by KsArT on 07.12.2024.
+//
+
+import Foundation
+
+let input = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+83: 17 5
+"""
+
+let date = Date.now
+
+let equations = inputTest.split(separator: "\n").map { $0.split(separator: ":") }
+
+let result1 = await calculateEquations(input: equations, operators: ["+", "*"])
+let result2 = await calculateEquations(input: equations, operators: ["+", "*", "|"])
+
+print("\ntime: \(Date.now.timeIntervalSince(date))")
+print("sum: \(result1)")
+print("sum: \(result2)")
+
+func calculateEquations(input: [[Substring.SubSequence]], operators: [String]) async -> Int {
+    await withTaskGroup(of: Int.self) { group -> Int in
+        for equation in equations {
+            group.addTask {
+                await getRightValue(equation, operators: operators)
+            }
+        }
+
+        return await group.reduce(0, +)
+    }
+}
+
+func getRightValue(_ equation: [Substring.SubSequence], operators: [String]) async -> Int {
+    guard equation.count == 2, let value = Int(equation[0]) else { return 0 }
+    let numbers = equation[1].split(separator: " ").compactMap({ Int($0) })
+    guard value > 0, !numbers.isEmpty else { return 0 }
+
+    let combinations = await generateOperatorCombinations(operators: operators, count: numbers.count - 1)
+
+    for combination in combinations {
+        if calculate(numbers: numbers, operators: combination) == value {
+            return value
+        }
+    }
+
+    return 0
+}
+
+func generateOperatorCombinations(operators: [String], count: Int) async  -> [[String]] {
+    guard count > 0, !operators.isEmpty else { return [[]] }
+
+    var result = [[String]](repeating: [], count: 1)
+    for _ in 0..<count {
+        var newResult = [[String]]()
+        for combination in result {
+            for op in operators {
+                newResult.append(combination + [op])
+            }
+        }
+        result = newResult
+    }
+    return result
+}
+
+func calculate(numbers: [Int], operators: [String]) -> Int {
+    var result = numbers[0]
+
+    for i in 1..<numbers.count {
+        result = calculateOperation(operators[i - 1], result, numbers[i])
+    }
+
+    return result
+}
+
+func calculateOperation(_ op: String, _ n1: Int, _ n2: Int) -> Int {
+    switch op {
+    case "+":
+        n1 + n2
+    case "*":
+        n1 * n2
+    case "|":
+        Int("\(n1)\(n2)") ?? 0
+    default:
+        n1
+    }
+}
