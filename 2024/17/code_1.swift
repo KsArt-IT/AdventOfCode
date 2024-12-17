99b8729e7bd02c9f7e1a4ddf5c9798868ad8d1e9
KsArT-IT <ksart.it@gmail.com>
2024-12-17 14:24:25 +0200

    feat: add code day 17 task 1

---
+++ 2024/17/code_1.swift
@@ -0,0 +1,105 @@
+//
+//  main.swift
+//  2024_17_1
+//
+//  Created by KsArT on 17.12.2024.
+//
+
+import Foundation
+
+let inputTest = """
+Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0
+"""
+
+let inputTest1 = """
+Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0
+"""
+
+// MARK: - Start
+if let (registerA, registerB, registerC, program) = parseInput(inputTest) {
+    let result = getOutputRunProgram(a: registerA, b: registerB, c: registerC, program: program)
+    print("Output: \(result)")
+} else {
+    print("Input string is incorrect!")
+}
+// MARK: - Func
+func getOutputRunProgram(a: Int, b: Int, c: Int, program: [Int]) -> String {
+    runProgram(a: a, b: b, c: c, program: program).map(String.init).joined(separator: ",")
+}
+
+func runProgram(a: Int, b: Int, c: Int, program: [Int]) -> [Int] {
+    var registerA = a
+    var registerB = b
+    var registerC = c
+    var instruction = 0
+    var output = [Int]()
+    let base = 8
+
+    func comboValue(_ operand: Int) -> Int {
+        switch operand {
+        case 0...3: operand
+        case 4: registerA
+        case 5: registerB
+        case 6: registerC
+        default: fatalError("Invalid combo operand: \(operand)")
+        }
+    }
+
+    while instruction < program.count {
+        guard program.count > 1 else {
+            fatalError("Invalid program length for instruction: \(instruction) < \(program.count)")
+        }
+        let opcode = program[instruction]
+        let operand = program[instruction + 1]
+
+        instruction += 2
+        switch opcode {
+        case 0: // adv
+            registerA /= Int(pow(2.0, Double(comboValue(operand))))
+
+        case 1: // bxl
+            registerB ^= operand
+
+        case 2: // bst
+            registerB = comboValue(operand) % base
+
+        case 3: // jnz
+            if registerA != 0 {
+                instruction = operand
+            }
+
+        case 4: // bxc
+            registerB ^= registerC
+
+        case 5: // out
+            output.append(comboValue(operand) % base)
+
+        case 6: // bdv
+            registerB = registerA / Int(pow(2.0, Double(comboValue(operand))))
+
+        case 7: // cdv
+            registerC = registerA / Int(pow(2.0, Double(comboValue(operand))))
+
+        default:
+            fatalError("Invalid opcode: \(opcode)")
+        }
+    }
+
+    return output
+}
+
+func parseInput(_ input: String) -> (Int, Int, Int, [Int])? {
+    let numbers = input.components(separatedBy: CharacterSet.decimalDigits.inverted)
+        .compactMap(Int.init)
+    guard numbers.count > 3 else { return nil }
+
+    return (numbers[0], numbers[1], numbers[2], Array(numbers[3...]))
+}
